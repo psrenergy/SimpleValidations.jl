01bb8369817c0de69b9c9d83fe2256daa541124a
pedroripper <pedros.ripper@gmail.com>
2023-10-05 16:32:12 -0300

    Cleanup

--- src/SimpleValidations.jl
+++ src/SimpleValidations.jl
@@ -4,16 +4,12 @@ using JSON
 using OrderedCollections
 import LoggingPolyglot as Log
 
-const _EXCLUDE_SYMBOLS = [Symbol(@__MODULE__), :eval, :include]
-
 export num_validation_errors, has_validation_errors, init_validations, validation_error, dump_validation_errors
 
 include("interface.jl")
 include("validations.jl")
 include("io.jl")
 
-
 const VALIDATIONS = Vector{ValidationError}()
-
 
 end # module SimpleValidations
--- src/io.jl
+++ src/io.jl
@@ -7,9 +7,7 @@ function _create_validation_dict(validation_error::ValidationError)
     )
 end
 
-function _create_validation_vector_dict()
-    return [_create_validation_dict(validation_error) for validation_error in VALIDATIONS]
-end
+_create_validation_vector_dict() =  [_create_validation_dict(validation_error) for validation_error in VALIDATIONS]
 
 function dump_validation_errors(path::String)
     Base.open(path, "w") do io
